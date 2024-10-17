8bc918ba043e03cbb09c2cc647fb78478a14e862
Elijah Jacob <elijahjacob3402@gmail.com>
2024-10-17 11:38:28 +0100

    just added variables in the js file

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// variabls
+
+//rock paper scissors btn
+const roct_btn = document.querySelector(`#rock-btn`);
+const paper_btn = document.querySelector(`#paper-btn`);
+const scissors_btn = document.querySelector(`#scissors-btn`);
+
+//outputs
+
+const score_output = document.querySelector(`#score-output`);
+const wins_output = document.querySelector(`#wins-output`);
+const loses_output = document.querySelector(`#loses-output`);
+const ties_output = document.querySelector(`#ties-output`);
+
+//moves
+
+const computer_move = document.querySelector(`#computer-move`);
+const player_move = document.querySelector(`#player-move`);
